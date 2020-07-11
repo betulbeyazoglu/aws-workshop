cd174da0eff61216983ae013d0a69dbe43ffbfc5
Betul <betul.nalan@gmail.com>
2020-07-11 10:43:55 -0400

    Added new version app.py

--- Projects/002-milliseconds-converter/app.py
+++ Projects/002-milliseconds-converter/app.py
@@ -7,10 +7,11 @@ not_valid=False
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template("index.html", developer_name=developer_name , not_valid=not_valid)
+    return render_template("index.html", developer_name=developer_name , not_valid=False)
 
 @app.route('/', methods=['POST'])
 def ms_converter():
+    time=""
     number=request.form['number']
     if number.isdigit():
         num=int(number)
@@ -19,30 +20,20 @@ def ms_converter():
             return render_template("result.html", milliseconds=number, result=f"just {msec} millisecond/s", developer_name=developer_name)
         elif num>=1000:
             hour=num//3600000
+            if hour:
+                time+=f"{hour} hour/s "
             min=(num%3600000)//60000
+            if min:
+                time+=f"{min} min/s "    
             sec=(num%60000)//1000
-            if hour!=0 and min!=0 and sec!=0:
-                return render_template("result.html", milliseconds=num, result=f"{hour} hour/s {min} minute/s {sec} second/s", developer_name=developer_name)
-            elif hour!=0 and min!=0 and sec==0:
-                return render_template("result.html", milliseconds=num, result=f"{hour} hour/s {min} minute/s", developer_name=developer_name)
-            elif hour!=0 and min==0 and sec!=0:
-                return render_template("result.html", milliseconds=num, result=f"{hour} hour/s {sec} second/s", developer_name=developer_name)
-            elif hour!=0 and min==0 and sec==0:
-                return render_template("result.html", milliseconds=num, result=f"{hour} hour/s", developer_name=developer_name)
-            elif hour==0 and min!=0 and sec!=0:
-                return render_template("result.html", milliseconds=num, result=f"{min} minute/s {sec} second/s", developer_name=developer_name)
-            elif hour==0 and min!=0 and sec==0:
-                return render_template("result.html", milliseconds=num, result=f"{min} minute/s", developer_name=developer_name)
-            elif hour==0 and min==0 and sec!=0:
-                return render_template("result.html", milliseconds=num, result=f"{sec} second/s", developer_name=developer_name)
-            
+            if sec:
+                time+=f"{sec} second/s"
+            return render_template("result.html", milliseconds=num, result=time, developer_name=developer_name)     
         else:
-            not_valid=True
-            return render_template("index.html", not_valid=not_valid, developer_name=developer_name)
+            return render_template("index.html", not_valid=True, developer_name=developer_name)
     else:
-        not_valid=True
-        return render_template("index.html", not_valid=not_valid, developer_name=developer_name)
+        return render_template("index.html", not_valid=True, developer_name=developer_name)
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=80)
-    #app.run(debug=True)
+    #app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
