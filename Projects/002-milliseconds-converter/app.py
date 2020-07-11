6a448a243f527c8dbc76918530f0c1895ce14950
Betul <betul.nalan@gmail.com>
2020-07-11 10:55:30 -0400

    Added new version of app.py

--- Projects/002-milliseconds-converter/app.py
+++ Projects/002-milliseconds-converter/app.py
@@ -34,5 +34,5 @@ def ms_converter():
         return render_template("index.html", not_valid=True, developer_name=developer_name)
 
 if __name__=='__main__':
-    #app.run(host='0.0.0.0', port=80)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
+    #app.run(debug=True)
